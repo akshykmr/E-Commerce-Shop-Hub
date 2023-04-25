04bb80e98ef73644cee72fc6fcf0f84c1d53a6d7
C1PH3R502 <tech24into07@gmail.com>
2023-04-25 18:23:04 +0530

    4th commit- str-api integrated

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,8 +2,22 @@ import Banner from "./Banner/Banner";
 import "./Home.scss";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
+import { useEffect } from "react";
+import {fetchDataFromApi} from '../../utils/api';
+
 
 const Home = () => {
+
+    useEffect(()  => {
+        getCategories()
+    }, []);
+
+    
+    const getCategories = () => {
+        fetchDataFromApi("/api/categories").then(res =>  console.log(res));
+    };
+
+
     return <div>
         <Banner/>
         <div className="main-content">
